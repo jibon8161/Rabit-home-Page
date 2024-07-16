fea0e8737832cc4912fbb7f3f896ba4aa2e2ffaf
jibon8161 <jibon2230@gmail.com>
2024-07-17 02:49:55 +0530

    done3

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PiTelegramLogo } from "react-icons/pi";
 import { BsTwitterX } from "react-icons/bs";
 import dlr from "./asset/logo.png";
 import img9 from "./asset/9.png";
 import img7 from "./asset/7.png";
-// import { BsFillFastForwardBtnFill } from "react-icons/bs";
+import "./style.css";
 import { FaYoutube } from "react-icons/fa";
 import { SiTiktok } from "react-icons/si";
+import { LuArrowBigDownDash } from "react-icons/lu";
+
 
 const Home = () => {
     const [isHovering, setIsHovering] = useState(false);
     const [isHovering1, setIsHovering1] = useState(false);
     const [isHovering2, setIsHovering2] = useState(false);
-    const [isHovering3, setIsHovering3] = useState(false);
+  const [isHovering3, setIsHovering3] = useState(false);
+  const [isHovered5, setIsHovered5] = useState(false);
+
   return (
     <div className="text-[#ff5f1f] ">
       <div className="container mx-auto px-4">
@@ -43,17 +47,14 @@ const Home = () => {
               <img src={dlr} alt="Dollar" className="w-[36rem]" />
             </div>
 
-            <p className="border-b-4 border-[#0000f7] shadow-xl shadow-white rounded-full  uppercase text-3xl font-extrabold text-[#0000f7] mt-4">
-              Rabit Star coin
+            <p className="border-b-4 border-[#0000f7] shadow-xl shadow-white rounded-full  uppercase text-3xl font-extrabold text-orange-600 mt-4">
+              <span className=""> Rabbit Star coin</span>
             </p>
           </div>
 
           <div className="col-span-2 mt-6">
             <button className="indicator">
-              <span className="indicator-item badge text-white text-sm mr-16">
-                Coming soon
-              </span>
-              <div className="border-2 p-4 rounded-full text-xl shadow-lg shadow-orange-600 text-center">
+              <div className="border-2 p-6 px-12 rounded-full text-xl shadow-lg shadow-orange-600 text-center">
                 RBSC
               </div>
             </button>
@@ -67,8 +68,8 @@ const Home = () => {
               <img src={dlr} alt="Dollar" className="w-[16rem]" />
             </div>
 
-            <p className="border-b-4 border-[#0000f7] shadow-xl shadow-white rounded-full  uppercase text-2xl font-extrabold text-[#0000f7] mt-4">
-              Rabit Star coin
+            <p className="border-b-4 border-[#0000f7] shadow-xl shadow-white rounded-full  uppercase text-2xl font-extrabold text-orange-600 mt-4">
+              Rabbit Star coin
             </p>
           </div>
           <div className="flex flex-wrap">
@@ -99,14 +100,9 @@ const Home = () => {
         </div>
 
         <div className="mt-12 lg:grid grid-cols-12 items-center">
-          <img
-            className="col-span-2 lg:block hidden max-w-full"
-            src={img9}
-            alt=""
-          />
-          <div className="col-span-8">
+          <div className="col-span-12">
             <p className="text-4xl lg:text-5xl text-center text-orange-600 uppercase font-extrabold">
-              Unleash your inner CEO
+              Discover Your Mastery <br /> Master the Crypto Exchange
             </p>
             <div className="flex lg:gap-8 justify-center md:mt-8 mt-8 mb-8 flex-wrap">
               <a
@@ -118,7 +114,7 @@ const Home = () => {
                 <FaYoutube />
                 {/* Conditional rendering of text */}
                 {isHovering1 && (
-                  <span className="absolute bg-black text-white text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
+                  <span className="absolute bg-black text-orange-600 text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
                     Youtube
                   </span>
                 )}
@@ -132,7 +128,7 @@ const Home = () => {
                 <SiTiktok />
                 {/* Conditional rendering of text */}
                 {isHovering && (
-                  <span className="absolute bg-black text-white text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
+                  <span className="absolute bg-black text-orange-600  text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
                     Tiktok
                   </span>
                 )}
@@ -146,7 +142,7 @@ const Home = () => {
                 <BsTwitterX />
                 {/* Conditional rendering of text */}
                 {isHovering2 && (
-                  <span className="absolute bg-black  text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8 ">
+                  <span className="absolute bg-black text-orange-600   text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8 ">
                     twitter
                   </span>
                 )}
@@ -157,23 +153,16 @@ const Home = () => {
                 onMouseEnter={() => setIsHovering3(true)}
                 onMouseLeave={() => setIsHovering3(false)}
               >
-                <PiTelegramLogo/>
+                <PiTelegramLogo />
                 {/* Conditional rendering of text */}
                 {isHovering3 && (
-                  <span className="absolute bg-black text-white text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
+                  <span className="absolute bg-black text-orange-600  text-xs py-1 px-2 rounded-md bottom-0 left-1/2 transform -translate-x-1/2 -mb-8">
                     Telegram
                   </span>
                 )}
               </a>
-           
-          
             </div>
           </div>
-          <img
-            className="col-span-2 lg:block hidden max-w-full"
-            src={img9}
-            alt=""
-          />
         </div>
 
         <div className="grid lg:grid-cols-12 items-center">
@@ -183,10 +172,10 @@ const Home = () => {
             src={img7}
             alt=""
           /> */}
-          <p className="col-span-full text-sm font-light text-center text-orange-600">
-            Make your way from the shaved hamster to the grandmaster CEO of the
-            tier-1 crypto exchange <br />
-            Buy upgrades, complete quests, invite friends and become the best
+          <p className="col-span-full text-xl font-light text-center text-orange-600">
+            Climb the ranks from a humble beginner to the esteemed Grand Master
+            of a tier-1 crypto exchange. <br /> Purchase upgrades, conquer
+            challenges, invite friends, and ascend to greatness.
           </p>
           {/* <img
             className="col-span-2 lg:block hidden max-w-full"
@@ -205,47 +194,112 @@ const Home = () => {
         </button> */}
         <a
           href="#_"
-          class="relative rounded-full inline-flex items-center justify-start py-3 pl-4 pr-12 w-full overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out shadow-lg shadow-white hover:pl-10 hover:pr-6 bg-[#0000f7] group hover:bg-orange-600 hover:shadow-orange-600 mt-12"
+          class="relative rounded-full inline-flex items-center justify-start py-5 pl-4 pr-12  lg:w-96  overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out shadow-lg shadow-white hover:pl-10 hover:pr-6 bg-orange-600 group hover:bg-orange-600 hover:shadow-orange-600 lg:mt-20 mt-12"
         >
           <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#171719] group-hover:h-full"></span>
 
-          <span class="relative w-full text-center transition-colors duration-200 ease-in-out group-hover:text-white text-2xl">
+          <span class="relative w-full text-center transition-colors duration-200 ease-in-out group-hover:text-white text-2xl uppercase text-black font-extrabold">
             Play Now
           </span>
         </a>
       </div>
-      <div className="lg:mt-36 mt-16 text-3xl">
-        <p className="">Roadmap</p>
-        <div className="container mx-auto p-6 w-full lg:flex justify-around items-center rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-12">
-          <p className="lg:text-6xl text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
-            March 2024
+      <div className="lg:mt-20 mt-8 text-3xl">
+        <div className="flex flex-col items-center">
+          <p className="lg:text-5xl font-bold mb-8">Conception</p>
+          <p className="text-8xl lg:mb-6">
+            <LuArrowBigDownDash />
           </p>
-          <div className="text-left text-base">
-            <li>Basic game</li>
-            <li>Mining updates</li>
-            <li>Earn tasks</li>
+        </div>
+        <div className="container mx-auto p-4 w-full lg:flex justify-evenly  items-start rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-6">
+          <div className="lg:w-1/4">
+            <p className="text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
+              November 2024
+            </p>
+          </div>
+          <div className="lg:w-3/4 text-left text-base">
+            <ul className="list-disc ml-5">
+              <li>Basic game</li>
+              <li>Earn tasks</li>
+              <li>Gift Card</li>
+            </ul>
           </div>
         </div>
-        <div className="container mx-auto p-6 w-full lg:flex justify-around items-center rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-8 mt-16">
-          <p className="lg:text-6xl text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
-            March 2024
-          </p>
-          <div className="text-left text-base">
-            <li>Basic game</li>
-            <li>Mining updates</li>
-            <li>Earn tasks</li>
+
+        <div className="container mx-auto p-4 w-full lg:flex justify-between items-start rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-12">
+          <div className="lg:w-1/4">
+            <p className="text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
+              October 2024
+            </p>
+          </div>
+          <div className="lg:w-3/4 text-left text-base">
+            <ul className="list-disc ml-5">
+              <li>Web 3 pre-listing Quest</li>
+              <li>Integrating wallets into the game</li>
+              <li>Building on-chain infrastructure</li>
+            </ul>
           </div>
         </div>
-        <div className="container mx-auto p-6 w-full lg:flex justify-around items-center rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-8 mt-16">
-          <p className="lg:text-6xl text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
-            March 2024
-          </p>
-          <div className="text-left text-base">
-            <li>Basic game</li>
-            <li>Mining updates</li>
-            <li>Earn tasks</li>
+
+        <div className="container mx-auto p-4 w-full lg:flex justify-between items-start rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-12">
+          <div className="lg:w-1/4">
+            <p className="text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
+              September 2024
+            </p>
+          </div>
+          <div className="lg:w-3/4 text-left text-base">
+            <ul className="list-disc ml-5">
+              <li>Force Rabbit Star</li>
+              <li>Token Generation</li>
+              <li>Launch of Token Utility In-Game</li>
+            </ul>
           </div>
         </div>
+
+        <div className="container mx-auto p-4 w-full lg:flex justify-between items-start rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-12">
+          <div className="lg:w-1/4">
+            <p className="text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
+              August 2024
+            </p>
+          </div>
+          <div className="lg:w-3/4 text-left text-base">
+            <ul className="list-disc ml-5">
+              <li>Rabbit Star Coin</li>
+              <li>Events with a time constraint</li>
+              <li>Live events</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="container mx-auto p-4 w-full lg:flex justify-between items-start rounded-lg shadow-lg shadow-orange-600 bg-orange-00 text-white lg:hover:text-white lg:hover:shadow-orange-600 bg-[#171719] hover:text-orange-600 lg:mt-5 mt-12">
+          <div className="lg:w-1/4">
+            <p className="text-4xl text-left mb-2 lg:mb-0 text-white lg:text-orange-600">
+              Start 2024
+            </p>
+          </div>
+          <div className="lg:w-3/4 text-left text-base">
+            <p>
+              Many more surprises await, <br /> but we'll keep them secret to
+              preserve your excitement.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2 p-2 border-orange-600 hover:border-white shadow-lg mt-20">
+        <a href="www" className="btn btn-circle mr-8">
+          <div className="rounded-full border-2 hover:border-orange-600 hover:animate-pulse p-4 ml-2 shadow-lg shadow-white">
+            <span className="text-red-600 text-4xl">
+              <FaYoutube />
+            </span>
+          </div>
+        </a>
+        <a href="https://www.google.com" className="btn btn-circle">
+          <div className="rounded-full border-2 hover:animate-pulse hover:border-orange-600 shadow-lg shadow-white p-4 ml-2">
+            <span className="text-orange-600 text-4xl">
+              <PiTelegramLogo />
+            </span>
+          </div>
+        </a>
+        <p className="text-center mt-2 border-b-2  border-white shadow-xl shadow-orange-600 rounded-full"></p>
       </div>
     </div>
   );
